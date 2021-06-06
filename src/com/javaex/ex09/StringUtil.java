45657b2e8306300871e4def9bb8b662c46b93c73
김민수 <minsu.kim1988@gmail.com>
2021-06-07 00:57:26 +0900

    06/June/2021

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,9 +2,11 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
+	public static String concatString = "";
+	
 	public static String concatString(String[] strArray){
 		
-		String concatString = "";
+		
 		for(int i=0; i<strArray.length; i++) {
 			concatString = concatString+strArray[i];
 		}
